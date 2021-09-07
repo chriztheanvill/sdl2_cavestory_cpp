16d698e606312d5cd956bd57264e3ae0c4b7fbf0
chriztheanvill <chriztheanvill@gmail.com>
2021-09-07 15:32:35 -0500

    testing_aa

--- 0000/main_cmake/src/Player.cpp
+++ 0000/main_cmake/src/Player.cpp
@@ -191,7 +191,7 @@ void Player::handleSlopeCollision(std::vector<Slope>& others) {
 		if (mGrounded) {
 			mPos.y = newY - mBoundingBox->getHeight( );
 
-			// Resuelve el bug 00
+			// Resuelve el bug asd
 		} else if (mPos.y >= newY - mBoundingBox->getHeight( )) {
 			mPos.y = newY - mBoundingBox->getHeight( );
 
